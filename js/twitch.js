6351010b2b1142504707a46a7be8c794f3a57501
Tim Harshman <goteamtim+git@gmail.com>
2016-09-05 13:26:33 -0700

    Twitch API work and styling

--- js/twitch.js
+++ js/twitch.js
@@ -1,9 +1,27 @@
 var apiUrl = "https://api.twitch.tv/kraken/streams/",
-    streamer = 'freecodecamp',
-    constStreamersList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  streamer = 'freecodecamp',
+  streamerList = document.getElementById('twitchList'),
+  constStreamersList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 
 
-$.getJSON(apiUrl + streamer + '?callback=?', function(data) {
-  console.log(data);
-});
+constStreamersList.forEach(function (streamer) {
+  $.getJSON(apiUrl + streamer + '?callback=?', function (data) {
+    var streamerDiv = document.createElement('li');
+    if(data.stream === null){
+      //User offline
+      //add 'offline class to li element
+      streamerDiv.className = 'offline';
+      console.log('offline: ' + data);
+      streamerDiv.innerHTML = streamer + ' is offline.';//Maybe take the user name and link to their profile.
+      
+    }else{
+      //User online, addd that class to element to style it
+      console.log('online: ' + data);
+      streamerDiv.className = 'online';
+      streamerDiv.innerHTML = 'Game: ' + data.stream.game;
+      streamerDiv.innerHTML += '<img src=\'' + data.stream.channel.logo + '\' style=\'height:48px;width:48px;\'></img>';
+    }
+    streamerList.appendChild(streamerDiv);
+  });
+}, this);
